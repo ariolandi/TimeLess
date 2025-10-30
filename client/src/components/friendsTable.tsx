ad4a2032a7d20407dd15589d5e8d9104e52324f2
Stella <stela_marinova98@abv.bg>
2025-10-30 13:57:54 +0200

    Build fix

--- client/src/components/friendsTable.tsx
+++ client/src/components/friendsTable.tsx
@@ -46,7 +46,7 @@ export default function FriendsTable({rows}: {rows: FriendData[]}) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (event: unknown, newPage: number) => {
+  const handleChangePage = (_event: unknown, newPage: number) => {
     setPage(newPage);
   };
 
