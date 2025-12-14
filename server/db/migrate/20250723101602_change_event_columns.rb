9ee61cfea1bf691c809d1466af71c8b71ee421cd
Stella <stela_marinova98@abv.bg>
2025-12-14 22:09:55 +0200

    migration fix

--- server/db/migrate/20250723101602_change_event_columns.rb
+++ server/db/migrate/20250723101602_change_event_columns.rb
@@ -1,9 +1,11 @@
-def change
-  remove_column :events, :title, :string
-  remove_column :events, :duration, :integer
-  remove_column :events, :end_time, :time
+class ChangeEventColumns < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :events, :title, :string
+    remove_column :events, :duration, :integer
+    remove_column :events, :end_time, :time
 
-  change_table :events do |t|
-    t.integer :day
+    change_table :events do |t|
+      t.integer :day
+    end
   end
 end
